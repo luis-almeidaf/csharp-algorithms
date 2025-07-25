3b8f08e18b66429077f64524077b73e358161beb
Luis <luis.almeida.faria@gmail.com>
2025-07-25 18:13:21 -0300

    feat: Add LeetCode 202 solution

---
+++ DsaChallenges/LeetCode/LeetCode202.cs
@@ -0,0 +1,34 @@
+﻿namespace DsaChallenges.LeetCode
+{
+    public class LeetCode202
+    {
+        public bool isHappy(int n)
+        {
+            HashSet<int> visited = new HashSet<int>();
+
+            while (!visited.Contains(n))
+            {
+                visited.Add(n);
+                n = SumOfSquares(n);
+
+                if (n == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static int SumOfSquares(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            return sum;
+        }
+    }
+}
