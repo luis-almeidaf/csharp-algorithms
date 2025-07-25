2ea7c1ef9da5bd30fd2258289fefe45e845aa726
Luis <luis.almeida.faria@gmail.com>
2025-07-25 18:13:38 -0300

    feat: Add LeetCode 1 solution

---
+++ DsaChallenges/LeetCode/LeetCode1.cs
@@ -0,0 +1,20 @@
+﻿namespace DsaChallenges.LeetCode
+{
+    public class LeetCode1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = [];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (dict.ContainsKey(diff))
+                {
+                    return [dict[diff], i];
+                }
+                dict.TryAdd(nums[i], i);
+            }
+            return [];
+        }
+    }
+}
