ca15e860428f7389a678ea3edfb897750e885ba3
Luis <luis.almeida.faria@gmail.com>
2025-08-01 21:17:26 -0300

    feat: LeetCode 1672

---
+++ src/DsaChallenges.Core/LeetCode/LeetCode1672.cs
@@ -0,0 +1,21 @@
+﻿namespace DsaChallenges.Core.LeetCode;
+
+public static class LeetCode1672
+{
+    public static int MaximumWealth(int[][] accounts)
+    {
+        var maxWealth = 0;
+        foreach (var account in accounts)
+        {
+            var sumWealth = 0;
+            foreach (var wealth in account)
+            {
+                sumWealth += wealth;
+                if (sumWealth > maxWealth)
+                    maxWealth = sumWealth;
+            }
+        }
+        return maxWealth;
+    }
+}
+
---
+++ src/DsaChallenges.Tests/LeetCode/LeetCode1672Tests.cs
@@ -0,0 +1,38 @@
+﻿using DsaChallenges.Core.LeetCode;
+
+namespace DsaChallenges.Tests.LeetCode;
+
+public class LeetCode1672Tests
+{
+    [Fact]
+    public void Should_ReturnMaximumWealth_WhenSingleCustomer()
+    {
+        int[][] accounts = [[1, 2, 3]];
+        var res = LeetCode1672.MaximumWealth(accounts);
+        Assert.Equal(6, res);
+    }
+
+    [Fact]
+    public void Should_ReturnMaximumWealth_WhenMultipleCustomers()
+    {
+        int[][] accounts = [[1, 5, 20], [5, 6, 7], [9, 10]];
+        var res = LeetCode1672.MaximumWealth(accounts);
+        Assert.Equal(26, res);
+    }
+
+    [Fact]
+    public void Should_ReturnCorrectWealth_WhenWealthIsSameForAllCustomers()
+    {
+        int[][] accounts = [[1, 2, 3], [1, 2, 3]];
+        var res = LeetCode1672.MaximumWealth(accounts);
+        Assert.Equal(6, res);
+    }
+
+    [Fact]
+    public void Should_ReturnZero_WhenAllAccountsAreEmpty()
+    {
+        int[][] accounts = [[], [], []];
+        var res = LeetCode1672.MaximumWealth(accounts);
+        Assert.Equal(0, res);
+    }
+}
