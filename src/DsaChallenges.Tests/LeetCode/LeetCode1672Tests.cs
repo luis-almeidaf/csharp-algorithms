2021574915105d3e7814c481a3733e9466402457
Luis <luis.almeida.faria@gmail.com>
2025-08-04 20:53:06 -0300

    refactor: Implementação das alterações sugeridas

--- src/DsaChallenges.Tests/LeetCode/LeetCode1672Tests.cs
+++ src/DsaChallenges.Tests/LeetCode/LeetCode1672Tests.cs
@@ -7,32 +7,32 @@ public class LeetCode1672Tests
     [Fact]
     public void Should_ReturnMaximumWealth_WhenSingleCustomer()
     {
-        int[][] accounts = [[1, 2, 3]];
-        var res = LeetCode1672.MaximumWealth(accounts);
-        Assert.Equal(6, res);
+        var accounts = new int[][] { [1, 2, 3] };
+        var result = LeetCode1672.MaximumWealth(accounts);
+        Assert.Equal(6, result);
     }
 
     [Fact]
     public void Should_ReturnMaximumWealth_WhenMultipleCustomers()
     {
-        int[][] accounts = [[1, 5, 20], [5, 6, 7], [9, 10]];
-        var res = LeetCode1672.MaximumWealth(accounts);
-        Assert.Equal(26, res);
+        var accounts = new int[][] { [1, 5, 20], [5, 6, 7], [9, 10] };
+        var result = LeetCode1672.MaximumWealth(accounts);
+        Assert.Equal(26, result);
     }
 
     [Fact]
     public void Should_ReturnCorrectWealth_WhenWealthIsSameForAllCustomers()
     {
-        int[][] accounts = [[1, 2, 3], [1, 2, 3]];
-        var res = LeetCode1672.MaximumWealth(accounts);
-        Assert.Equal(6, res);
+        var accounts = new int[][] { [1, 2, 3], [1, 2, 3] };
+        var result = LeetCode1672.MaximumWealth(accounts);
+        Assert.Equal(6, result);
     }
 
     [Fact]
     public void Should_ReturnZero_WhenAllAccountsAreEmpty()
     {
-        int[][] accounts = [[], [], []];
-        var res = LeetCode1672.MaximumWealth(accounts);
-        Assert.Equal(0, res);
+        var accounts = new int[][] { [], [], [] };
+        var result = LeetCode1672.MaximumWealth(accounts);
+        Assert.Equal(0, result);
     }
 }
