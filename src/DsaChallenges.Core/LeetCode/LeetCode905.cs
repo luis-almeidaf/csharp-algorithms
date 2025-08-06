90c98150843e22c2feb13c72bae6e7203b03c0b6
Luis <luis.almeida.faria@gmail.com>
2025-08-05 21:04:17 -0300

    feat: LeetCode 905

---
+++ src/DsaChallenges.Core/LeetCode/LeetCode905.cs
@@ -0,0 +1,26 @@
+﻿namespace DsaChallenges.Core.LeetCode
+{
+    public static class LeetCode905
+    {
+        public static int[] SortArrayByParity(int[] nums)
+        {
+            var result = new int[nums.Length];
+            int right = nums.Length - 1;
+            int left = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    result[left] = nums[i];
+                    left++;
+                }
+                else
+                {
+                    result[right] = nums[i];
+                    right--;
+                }
+            }
+            return result;
+        }
+    }
+}
