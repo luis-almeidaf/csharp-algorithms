cc5f22d8e69bafac6ef406a1e39d75e418352a99
Luis <luis.almeida.faria@gmail.com>
2025-08-05 21:12:00 -0300

    feat: LeetCode912

---
+++ src/DsaChallenges.Core/LeetCode/LeetCode912.cs
@@ -0,0 +1,43 @@
+﻿namespace DsaChallenges.Core.LeetCode;
+
+public static class LeetCode912
+{
+    public static int[] SortArray(int[] nums)
+    {
+        Quicksort(nums, 0, nums.Length - 1);
+        return nums;
+    }
+
+    private static void Quicksort(int[] arr, int begin, int end)
+    {
+        if (begin < end)
+        {
+            var rand = new Random();
+            var pivotIndex = rand.Next(begin, end + 1);
+            (arr[pivotIndex], arr[end]) =  (arr[end], arr[pivotIndex]);
+                                 
+            var partIndex = Partition(arr, begin, end);
+            Quicksort(arr, begin, partIndex - 1);
+            Quicksort(arr, partIndex + 1, end);
+        }
+    }
+
+    private static int Partition(int[] arr, int begin, int end)
+    {
+        var pivot = arr[end];
+        var i = (begin - 1);
+
+        for (var j = begin; j < end; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                (arr[i], arr[j]) = (arr[j], arr[i]);
+            }
+        }
+
+        (arr[i + 1], arr[end]) = (arr[end], arr[i + 1]);
+
+        return i + 1;
+    }
+}
---
+++ src/DsaChallenges.Tests/LeetCode/LeetCode912Tests.cs
@@ -0,0 +1,17 @@
+﻿using DsaChallenges.Core.LeetCode;
+
+namespace DsaChallenges.Tests.LeetCode
+{
+    public class LeetCode912Tests
+    {
+        [Theory]
+        [InlineData(new int[] { 5, 2, 3, 1 }, new int[] { 1, 2, 3, 5 })]
+        [InlineData(new int[] { 100, 900, 3, 1, 2, 10, 0 }, new int[] { 0, 1, 2, 3, 10, 100, 900 })]
+        [InlineData(new int[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1 }, new int[] { 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 })]
+        public void ShouldSortArrayInAscendingOrder_WhenUnsortedArrayIsGiven(int[] nums, int[] expectedResult)
+        {
+            var result = LeetCode912.SortArray(nums);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
