73e1505403b8b88983fb54e4e9a5713b161491a6
Luis <140249169+luis-almeidaf@users.noreply.github.com>
2025-08-13 18:20:24 -0300

    feat: leetcode 2942 (#7)

---
+++ src/DsaChallenges.Core/LeetCode/LeetCode2942.cs
@@ -0,0 +1,18 @@
+﻿namespace Challenges.src.LeetCode
+{
+    public static class LeetCode2942
+    {
+        public static IList<int> FindWordsContaining(string[] words, char x)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains(x))
+                {
+                    list.Add(i);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ src/DsaChallenges.Tests/LeetCode/LeetCode2942Tests.cs
@@ -0,0 +1,29 @@
+﻿using Challenges.src.LeetCode;
+
+namespace DsaChallenges.Tests.LeetCode
+{
+    public class LeetCode2942Tests
+    {
+        [Fact]
+        public void Should_Return_Indices_Of_Words_Containing_Char()
+        {
+            var words = new string[] { "leet", "code" };
+            char x = 'e';
+            var expected = new List<int> { 0, 1 };
+
+            var result = LeetCode2942.FindWordsContaining(words, x);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Should_Return_Empty_List_When_No_Words_Contain_Char()
+        {
+            var words = new string[] { "teste", "retorno" };
+            char x = 'u';
+            var expected = new List<int> { };
+
+            var result = LeetCode2942.FindWordsContaining(words, x);
+            Assert.Equal(expected, result);
+        }
+    }
+}
