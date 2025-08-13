6bbef0d52fdbcc800b7137d7fe25d503072f9871
Luis <luis.almeida.faria@gmail.com>
2025-08-13 17:51:53 -0300

    feat: LeetCode-2011

---
+++ src/DsaChallenges.Core/LeetCode/LeetCode2011.cs
@@ -0,0 +1,22 @@
+﻿namespace Challenges.src.LeetCode
+{
+    public static class LeetCode2011
+    {
+        public static int FinalValueAfterOperations(string[] operations)
+        {
+            int count = 0;
+            for (int i = 0; i < operations.Length; i++)
+            {
+                if (operations[i].Contains('+'))
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ src/DsaChallenges.Tests/LeetCode/LeetCode2011Tests.cs
@@ -0,0 +1,19 @@
+﻿using Challenges.src.LeetCode;
+
+namespace DsaChallenges.Tests.LeetCode
+{
+    public class LeetCode2011Tests
+    {
+        [Theory]
+        [InlineData(new string[] { "--X", "X++", "X++" }, 1)]
+        [InlineData(new string[] { "--X", "X--", "X++" }, -1)]
+        [InlineData(new string[] { "++X", "X++", "X++" }, 3)]
+        [InlineData(new string[] { "--X", "X++", "X++", "X--" }, 0)]
+        [InlineData(new string[] { "--Teste", "++", "Teste++", "-", "++++" }, 1)]
+        public void Should_Return_Correct_Value_When_Multiple_Increments_And_Decrements(string[] operations, int expected)
+        {
+            var result = LeetCode2011.FinalValueAfterOperations(operations);
+            Assert.Equal(expected, result);
+        }
+    }
+}
