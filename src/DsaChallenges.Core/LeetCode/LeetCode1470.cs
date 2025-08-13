3ed249969ffb3e1038dceb3d8c37ad63674a6199
Luis <luis.almeida.faria@gmail.com>
2025-08-13 17:25:09 -0300

    feat: LeetCode 1470

---
+++ src/DsaChallenges.Core/LeetCode/LeetCode1470.cs
@@ -0,0 +1,16 @@
+﻿namespace DsaChallenges.Core.LeetCode;
+
+public static class LeetCode1470
+{
+    public static int[] Shuffle(int[] nums, int n)
+    {
+        int[] result = new int[nums.Length];
+
+        for (int i = 0; i < n; i++)
+        {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[i + n];
+        }
+        return result;
+    }
+}
---
+++ src/DsaChallenges.Tests/LeetCode/LeetCode1470Tests.cs
@@ -0,0 +1,15 @@
+﻿using DsaChallenges.Core.LeetCode;
+
+namespace DsaChallenges.Tests.LeetCode;
+
+public class LeetCode1470Tests
+{
+    [Fact]
+    public void ShouldShuffleTheArrayBasedOnTheInput()
+    {
+        int n = 3;
+        var nums = new int[] { 2, 5, 1, 3, 4, 7 };
+        var result = LeetCode1470.Shuffle(nums, n);
+        Assert.Equal([2, 3, 5, 4, 1, 7], result);
+    }
+}
