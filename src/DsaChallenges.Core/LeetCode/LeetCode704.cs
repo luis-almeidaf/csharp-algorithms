aebf9995416373348323657d1f0ecad04c45999e
Luis <luis.almeida.faria@gmail.com>
2025-08-26 20:32:43 -0300

    feat: LeetCode 704

---
+++ src/DsaChallenges.Core/LeetCode/LeetCode704.cs
@@ -0,0 +1,29 @@
+﻿namespace DsaChallenges.Core.LeetCode;
+
+public static class LeetCode704
+{
+    public static int Search(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length -1;
+
+        while (left <= right)
+        {
+            var mid = (left + right) / 2;
+            var guess = nums[mid];
+            if (guess == target)
+            {
+                return mid;
+            }
+            if (guess < target)
+            {
+                left = mid + 1;
+            }
+            if (guess > target)
+            {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/DsaChallenges.Tests/LeetCode/LeetCode704Tests.cs
@@ -0,0 +1,25 @@
+﻿using DsaChallenges.Core.LeetCode;
+
+namespace DsaChallenges.Tests.LeetCode;
+
+public class LeetCode704Tests
+{
+    [Theory]
+    [InlineData(new int[] { -1, 0, 3, 5, 9, 12 }, 9, 4)]
+    [InlineData(new int[] { -5, 0, 1, 4, 10, 12, 14,17, 20 }, 0, 1)] 
+    public void Should_ReturnIndex_When_TargetExists(int[] nums, int target, int expectedResult)
+    {
+        var result = LeetCode704.Search(nums, target);
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void Should_ReturnMinusOne_When_TargetNotFound()
+    {
+        int[] nums = [-1, 0, 3, 5, 9, 12];
+        var target = 2;
+        var result = LeetCode704.Search(nums, target);
+        Assert.Equal(-1, result);
+    }
+    
+}
