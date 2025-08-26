38ba89015e2603fc28d520e24124c7695bc21a18
Luis <140249169+luis-almeidaf@users.noreply.github.com>
2025-08-25 23:06:45 -0300

    feat: Leetcode 2161 (#8)

---
+++ src/DsaChallenges.Core/LeetCode/LeetCode2161.cs
@@ -0,0 +1,32 @@
+﻿namespace Challenges.src.LeetCode
+{
+    public static class LeetCode2161
+    {
+        public static int[] PivotArray(int[] nums, int pivot)
+        {
+            int[] result = new int[nums.Length];
+            int left = 0;
+            int right = nums.Length - 1;
+
+            for (int i = 0, j = nums.Length - 1; i < nums.Length; i++, j--)
+            {
+                if (nums[i] < pivot)
+                {
+                    result[left] = nums[i];
+                    left++;
+                }
+                if (nums[j] > pivot)
+                {
+                    result[right] = nums[j];
+                    right--;
+                }
+            }
+            while (left <= right)
+            {
+                result[left] = pivot;
+                left++;
+            }
+            return result;
+        }
+    }
+}
