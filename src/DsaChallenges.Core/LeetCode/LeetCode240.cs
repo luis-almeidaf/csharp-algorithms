70ff58231c12307d0a5185ca73b8acaf883beab4
Luis <luis.almeida.faria@gmail.com>
2025-08-28 20:34:18 -0300

    feat: LeetCode 240

---
+++ src/DsaChallenges.Core/LeetCode/LeetCode240.cs
@@ -0,0 +1,22 @@
+﻿namespace DsaChallenges.Core.LeetCode;
+
+public static class LeetCode240
+{
+    public static bool SearchMatrix(int[][] matrix, int target)
+    {
+        int row = 0;
+        int cols = matrix[0].Length -1;
+
+        while (row < matrix.Length && cols >= 0)
+        {
+            if (target == matrix[row][cols])
+                return true;
+            else if (target > matrix[row][cols])
+                row++;
+            else
+                cols--;
+        }
+        return false;
+    }
+
+}
