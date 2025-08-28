9a56410285988d3981d1ea2d5d6dc6ea674b56cc
Luis <140249169+luis-almeidaf@users.noreply.github.com>
2025-08-28 20:36:30 -0300

    feat: LeetCode 240 (#11)

---
+++ src/DsaChallenges.Tests/LeetCode/LeetCode240Tests.cs
@@ -0,0 +1,24 @@
+﻿using DsaChallenges.Core.LeetCode;
+
+namespace DsaChallenges.Tests.LeetCode;
+
+public class LeetCode240Tests
+{
+    [Fact]
+    public void Should_Return_True_When_Target_Is_Found()
+    {
+        int[][] matrix = [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]];
+        var target = 19;
+        var result = LeetCode240.SearchMatrix(matrix, target);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Should_Return_False_When_Target_Is_Not_Found()
+    {
+        int[][] matrix = [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]];
+        var target = 100;
+        var result = LeetCode240.SearchMatrix(matrix, target);
+        Assert.False(result);
+    }
+}
